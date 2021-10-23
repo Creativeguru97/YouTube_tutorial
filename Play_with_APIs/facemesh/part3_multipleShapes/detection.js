a78ec9121a5cb12e651028bc09f380c8dbdee42b
kazcodingburner <40142129+Creativeguru97@users.noreply.github.com>
2021-10-23 23:18:30 +0900

    Add part3 sample code

---
+++ Play_with_APIs/facemesh/part3_multipleShapes/detection.js
@@ -0,0 +1,28 @@
+let detections = [];
+
+const videoElement = document.getElementById('video');
+
+function gotFaces(results) {
+  detections = results;
+  // console.log(detections);
+}
+
+const faceMesh = new FaceMesh({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+}});
+
+faceMesh.setOptions({
+  maxNumFaces: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+faceMesh.onResults(gotFaces);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await faceMesh.send({image: videoElement});
+  },
+  width: 640,
+  height: 480
+});
+camera.start();
---
+++ Play_with_APIs/facemesh/part3_multipleShapes/sketch.js
@@ -0,0 +1,136 @@
+let sketch = function(p){
+  let canvas;
+  let dMouse = [];
+  let closest = 0;
+  let isEditMode = false;
+
+  let shapes = [[]];
+  /*
+  let sample2DArray = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]
+  */
+
+  let shapeIndex = 0;
+
+  p.setup = function(){
+    canvas = p.createCanvas(640, 480);
+    canvas.id('canvas');
+
+    p.colorMode(p.HSB);
+  }
+
+  p.draw = function(){
+    p.clear();
+    if(detections != undefined){
+      if(detections.multiFaceLandmarks != undefined && detections.multiFaceLandmarks.length >= 1){
+        p.drawShapes();
+        if(isEditMode == true) p.faceMesh();
+        p.glow();
+      }
+    }
+  }
+
+  p.faceMesh = function(){
+    p.stroke(255);
+    p.strokeWeight(3);
+
+    p.beginShape(p.POINTS);
+    for(let i=0; i<detections.multiFaceLandmarks[0].length; i++){
+      let x = detections.multiFaceLandmarks[0][i].x * p.width;
+      let y = detections.multiFaceLandmarks[0][i].y * p.height;
+      p.vertex(x, y);
+
+      let d = p.dist(x, y, p.mouseX, p.mouseY);
+      dMouse.push(d);
+    }
+    p.endShape();
+
+    let minimum = p.min(dMouse);
+    closest = dMouse.indexOf(minimum);
+
+    p.stroke(0, 100, 100);
+    p.strokeWeight(10);
+    p.point(
+      detections.multiFaceLandmarks[0][closest].x * p.width,
+      detections.multiFaceLandmarks[0][closest].y * p.height
+    );
+
+    dMouse.splice(0, dMouse.length);
+  }
+
+  p.mouseClicked = function(){
+    if(isEditMode == true) shapes[shapeIndex].push(closest);
+    console.log(shapes);
+  }
+
+  p.drawShapes = function(){
+    for(let s = 0; s < shapes.length; s++){
+      if(s == shapeIndex) p.fill(0, 0, 50);
+      else p.fill(0, 0, 0);
+      p.stroke(0, 0, 100);
+      p.strokeWeight(3);
+
+      p.beginShape();
+        for(let i = 0; i < shapes[s].length; i++){
+          p.vertex(
+            detections.multiFaceLandmarks[0][shapes[s][i]].x * p.width,
+            detections.multiFaceLandmarks[0][shapes[s][i]].y * p.height,
+          );
+        }
+      p.endShape();
+    }
+  }
+
+  p.keyTyped = function(){
+    if(p.key === 'e') isEditMode = !isEditMode;
+
+    if(p.key === 'c'){
+      if(shapes[shapes.length-1].length > 0){
+        shapes.push([]);
+        shapeIndex++;
+      }
+      console.log(shapes);
+    }
+
+    if(p.key === 'z'){
+      if(shapes[shapeIndex] != undefined){
+        if(shapes[shapeIndex].length > 0) shapes[shapeIndex].pop();
+      }
+      console.log(shapes[shapeIndex]);
+    }
+
+    if(p.key === 'd'){
+      shapes = [[]];
+      shapeIndex = 0;
+      console.log(shapes);
+    }
+  }
+
+  p.keyPressed = function(){
+    if(p.keyCode === p.UP_ARROW){
+      if(shapes[shapeIndex] != undefined){
+        if(shapes[shapeIndex].length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
+        if(shapeIndex < shapes.length-1) shapeIndex++;
+      }
+    } else if(p.keyCode === p.DOWN_ARROW){
+      if(shapes[shapeIndex] != undefined){
+        if(shapes[shapeIndex].length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
+        if(shapeIndex > 0) shapeIndex--;
+      }
+    }
+    console.log(shapeIndex);
+  }
+
+  p.glow = function(){
+    p.drawingContext.shadowOffsetX = 0;
+    p.drawingContext.shadowOffsetY = 0;
+    p.drawingContext.shadowBlur = 20;
+    p.drawingContext.shadowColor = 'rgba(255, 255, 255, 100)';
+  }
+}
+
+let myp5 = new p5(sketch);
