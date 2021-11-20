af571b1812659eafd44ecc63f5c0439360f2a961
kazcodingburner <40142129+Creativeguru97@users.noreply.github.com>
2021-11-20 13:49:11 +0900

    Improve final version

--- Play_with_APIs/facemesh/final/detection.js
+++ Play_with_APIs/facemesh/final/detection.js
@@ -22,7 +22,7 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await faceMesh.send({image: videoElement});
   },
-  width: 640,
-  height: 480
+  width: 1280,//Or 640
+  height: 720 // Or 480
 });
 camera.start();
--- Play_with_APIs/facemesh/final/sketch.js
+++ Play_with_APIs/facemesh/final/sketch.js
@@ -33,143 +33,128 @@ let sketch = function(p){
   let shapeIndex = 0;
   let tParameters;
   let capture;
-
   let shapesData;
-  let isDragOver = false;
-
-  // p.preload = function(){
-  //   shapesData = p.loadJSON("drawings/firstDraw.json");
-  // }
-
-  let isMouseOver = false;
+  let isDraggedOver = false;
 
   p.setup = function(){
-    canvas = p.createCanvas(640, 480);
+    canvas = p.createCanvas(1280, 720);//Or (640, 480)
     canvas.id('canvas');
-    canvas.mouseOver(() => {isMouseOver = true});
-    canvas.mouseOut(() => {isMouseOver = false});
-
     canvas.dragOver(() => {
-      isDragOver = true;
+      isDraggedOver = true;
     });
     canvas.dragLeave(() => {
-      isDragOver = false;
+      isDraggedOver = false;
     });
 
     canvas.drop((file) => {
       if(file.subtype == 'json'){
         shapes = file.data.shapes;
         shapeIndex = shapes.length-1;
-        isDragOver = false;
+        isDraggedOver = false;
       }else{
-        isDragOver = false;
+        isDraggedOver = false;
       }
     });
 
     p.colorMode(p.HSB, 360, 100, 100, 100);
 
-    // --- Edit button ---
     edit_button = p.createButton("Edit mode off");
     edit_button.mousePressed(p.toggleEdit);
-    edit_button.class('Buttons');
-    edit_button.id('edit_button');
+    edit_button.class("Buttons");
+    edit_button.id("edit_button");
 
-    // --- Sliders ---
     fill_H_Value = p.createDiv();
     fill_H_Value.class('valueDisplay');
     fill_H_Slider = p.createSlider(0, 360, p.random(360), 5);
     fill_H_Slider.class('Slider');
-    fill_H_Slider.input(() => {p.editShapes('fill_H', fill_H_Slider.value())});
 
     fill_S_Value = p.createDiv();
     fill_S_Value.class('valueDisplay');
     fill_S_Slider = p.createSlider(0, 100, 50, 5);
     fill_S_Slider.class('Slider');
-    fill_S_Slider.input(() => {p.editShapes('fill_S', fill_S_Slider.value())});
 
     fill_B_Value = p.createDiv();
     fill_B_Value.class('valueDisplay');
     fill_B_Slider = p.createSlider(0, 100, 100, 5);
     fill_B_Slider.class('Slider');
-    fill_B_Slider.input(() => {p.editShapes('fill_B', fill_B_Slider.value())});
 
     fill_O_Value = p.createDiv();
     fill_O_Value.class('valueDisplay');
     fill_O_Slider = p.createSlider(0, 100, 100, 5);
     fill_O_Slider.class('Slider');
-    fill_O_Slider.input(() => {p.editShapes('fill_O', fill_O_Slider.value())});
 
     stroke_H_Value = p.createDiv();
     stroke_H_Value.class('valueDisplay');
     stroke_H_Slider = p.createSlider(0, 360, p.random(360), 5);
     stroke_H_Slider.class('Slider');
-    stroke_H_Slider.input(() => {p.editShapes('stroke_H', stroke_H_Slider.value())});
 
     stroke_S_Value = p.createDiv();
     stroke_S_Value.class('valueDisplay');
     stroke_S_Slider = p.createSlider(0, 100, 50, 5);
     stroke_S_Slider.class('Slider');
-    stroke_S_Slider.input(() => {p.editShapes('stroke_S', stroke_S_Slider.value())});
 
     stroke_B_Value = p.createDiv();
     stroke_B_Value.class('valueDisplay');
     stroke_B_Slider = p.createSlider(0, 100, 100, 5);
     stroke_B_Slider.class('Slider');
-    stroke_B_Slider.input(() => {p.editShapes('stroke_B', stroke_B_Slider.value())});
 
     stroke_O_Value = p.createDiv();
     stroke_O_Value.class('valueDisplay');
     stroke_O_Slider = p.createSlider(0, 100, 100, 5);
     stroke_O_Slider.class('Slider');
-    stroke_O_Slider.input(() => {p.editShapes('stroke_O', stroke_O_Slider.value())});
 
-    // --- Image buttons ---
-    screenshot_button = p.createButton('');
+    screenshot_button = p.createButton("");
     screenshot_button.mousePressed(p.screenShot);
-    screenshot_button.class('imageButtons');
-    screenshot_button.id('screenshot_button');
+    screenshot_button.class("imageButtons");
+    screenshot_button.id("screenshot_button");
 
-    save_drawing_button = p.createButton('');
+    save_drawing_button = p.createButton("");
     save_drawing_button.mousePressed(p.saveDrawing);
-    save_drawing_button.class('imageButtons');
-    save_drawing_button.id('save_drawing_button');
+    save_drawing_button.class("imageButtons");
+    save_drawing_button.id("save_drawing_button");
 
-    index_UP_button = p.createButton('');
+    index_UP_button = p.createButton("");
     index_UP_button.mousePressed(p.upIndex);
-    index_UP_button.class('imageButtons');
-    index_UP_button.id('index_UP_button');
+    index_UP_button.class("imageButtons");
+    index_UP_button.id("index_UP_button");
 
-    index_DOWN_button = p.createButton('');
+    index_DOWN_button = p.createButton("");
     index_DOWN_button.mousePressed(p.downIndex);
-    index_DOWN_button.class('imageButtons');
-    index_DOWN_button.id('index_DOWN_button');
+    index_DOWN_button.class("imageButtons");
+    index_DOWN_button.id("index_DOWN_button");
 
-    // --- Text buttons ---
     complete_button = p.createButton("complete");
     complete_button.mousePressed(p.complete);
-    complete_button.class('Buttons');
-    complete_button.id('complete_button');
+    complete_button.class("Buttons");
+    complete_button.id("complete_button");
 
     undo_button = p.createButton("undo");
     undo_button.mousePressed(p.undo);
-    undo_button.class('Buttons');
-    undo_button.id('undo_button');
+    undo_button.class("Buttons");
+    undo_button.id("undo_button");
 
     delete_button = p.createButton("delete");
     delete_button.mousePressed(p.deleteDrawing);
-    delete_button.class('Buttons');
-    delete_button.id('delete_button');
+    delete_button.class("Buttons");
+    delete_button.id("delete_button");
+
+    tParameters = {
+      fill_H : fill_H_Slider.value(),
+      fill_S : fill_S_Slider.value(),
+      fill_B : fill_B_Slider.value(),
+      fill_O : fill_O_Slider.value(),
+      stroke_H : stroke_H_Slider.value(),
+      stroke_S : stroke_S_Slider.value(),
+      stroke_B : stroke_B_Slider.value(),
+      stroke_O : stroke_O_Slider.value()
+    }
 
     capture = p.createCapture(p.VIDEO);
     capture.size(p.width, p.height);
     capture.hide();
 
-    // shapes = shapesData.shapes;
-    // console.log(shapes);
-    // shapeIndex = shapes.length-1;
-
     p.textAlign(p.CENTER, p.CENTER);
-    p.textSize(16);
+    p.textSize(24);
   }
 
   p.draw = function(){
@@ -181,15 +166,14 @@ let sketch = function(p){
           p.faceMesh();
           p.editShapes();
         }
+        p.glow();
 
-        console.log()
-
-        if(isDragOver == true){
+        if(isDraggedOver == true){
           p.noStroke();
           p.fill(0, 0, 100, 10);
           p.rect(0, 0, p.width, p.height);
           p.fill(0, 0, 100);
-          p.text('Drag your drawing here.', p.width/2, p.height/2);
+          p.text('Drag your drawing here', p.width/2, p.height/2);
         }
       }
     }
@@ -206,7 +190,7 @@ let sketch = function(p){
   }
 
   p.faceMesh = function(){
-    p.stroke(255);
+    p.stroke(0, 0, 100);
     p.strokeWeight(3);
 
     p.beginShape(p.POINTS);
@@ -234,9 +218,13 @@ let sketch = function(p){
   }
 
   p.mouseClicked = function(){
-    if(isMouseOver == true){
-      shapes[shapeIndex].indices.push(closest);
-      console.log(shapes);
+    if(p.mouseX >= 0 && p.mouseX <= p.width){
+      if(p.mouseY >= 0 && p.mouseY <= p.height){
+        if(isEditMode == true){
+          shapes[shapeIndex].indices.push(closest);
+          console.log(shapes);
+        }
+      }
     }
   }
 
@@ -274,8 +262,42 @@ let sketch = function(p){
     }
   }
 
-  p.editShapes = function(properties, newValue){
-    shapes[shapeIndex][properties] = newValue;
+  p.editShapes = function(){
+    // --- fill ---
+    if(tParameters.fill_H != fill_H_Slider.value()){
+      tParameters.fill_H = fill_H_Slider.value();
+      shapes[shapeIndex].fill_H = fill_H_Slider.value();
+    }
+    if(tParameters.fill_S!= fill_S_Slider.value()){
+      tParameters.fill_S = fill_S_Slider.value();
+      shapes[shapeIndex].fill_S = fill_S_Slider.value();
+    }
+    if(tParameters.fill_B != fill_B_Slider.value()){
+      tParameters.fill_B = fill_B_Slider.value();
+      shapes[shapeIndex].fill_B = fill_B_Slider.value();
+    }
+    if(tParameters.fill_O != fill_O_Slider.value()){
+      tParameters.fill_O = fill_O_Slider.value();
+      shapes[shapeIndex].fill_O = fill_O_Slider.value();
+    }
+
+    // --- stroke ---
+    if(tParameters.stroke_H != stroke_H_Slider.value()){
+      tParameters.stroke_H = stroke_H_Slider.value();
+      shapes[shapeIndex].stroke_H = stroke_H_Slider.value();
+    }
+    if(tParameters.stroke_S != stroke_S_Slider.value()){
+      tParameters.stroke_S = stroke_S_Slider.value();
+      shapes[shapeIndex].stroke_S = stroke_S_Slider.value();
+    }
+    if(tParameters.stroke_B != stroke_B_Slider.value()){
+      tParameters.stroke_B = stroke_B_Slider.value();
+      shapes[shapeIndex].stroke_B = stroke_B_Slider.value();
+    }
+    if(tParameters.stroke_O != stroke_O_Slider.value()){
+      tParameters.stroke_O = stroke_O_Slider.value();
+      shapes[shapeIndex].stroke_O = stroke_O_Slider.value();
+    }
   }
 
   p.keyTyped = function(){
@@ -286,13 +308,21 @@ let sketch = function(p){
     if(p.key === 's') p.screenShot();
     if(p.key === 'j') p.saveDrawing();
   }
+
   p.toggleEdit = function(){
     isEditMode = !isEditMode;
 
+    if(isEditMode == true){
+      edit_button.html("Edit mode on");
+    }else if(isEditMode == false){
+      edit_button.html("Edit mode off");
+    }
   }
+
   p.complete = function(){
     if(shapes[shapes.length-1].indices.length > 0){
       if(shapes[shapeIndex].indices.length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
+
       shapes.push(
         {
           fill_H : p.random(360),
@@ -310,55 +340,64 @@ let sketch = function(p){
     }
     console.log(shapes);
   }
+
   p.undo = function(){
     if(shapes[shapeIndex] != undefined){
       if(shapes[shapeIndex].indices.length > 0) shapes[shapeIndex].indices.pop();
     }
     console.log(shapes[shapeIndex].indices);
   }
+
   p.deleteDrawing = function(){
-    shapes = [{
-      fill_H : p.random(360),
-      fill_S : 50,
-      fill_B : 100,
-      fill_O : 100,
-      stroke_H : p.random(360),
-      stroke_S : 50,
-      stroke_B : 100,
-      stroke_O : 100,
-      indices : []
-    }]
+    shapes = [
+      {
+        fill_H : p.random(360),
+        fill_S : 50,
+        fill_B : 100,
+        fill_O : 100,
+        stroke_H : p.random(360),
+        stroke_S : 50,
+        stroke_B : 100,
+        stroke_O : 100,
+        indices : []
+      }
+    ];
     shapeIndex = 0;
     console.log(shapes);
   }
+
   p.screenShot = function(){
     p.image(capture.get(0, 0, p.width, p.height), 0, 0, p.width, p.height);
     p.drawShapes();
     p.glow();
-    p.saveCanvas('screenshot', 'png');
+    p.saveCanvas('screenShot', 'png');
   }
+
   p.saveDrawing = function(){
     let s = {shapes};
     p.saveJSON(s, 'untitled_shapes.json');
   }
 
-
   p.keyPressed = function(){
     if(p.keyCode === p.UP_ARROW) p.upIndex();
     else if(p.keyCode === p.DOWN_ARROW) p.downIndex();
   }
+
   p.upIndex = function(){
     if(shapes[shapeIndex] != undefined){
-        if(shapes[shapeIndex].indices.length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
-        if(shapeIndex < shapes.length-1) shapeIndex++;
-        p.resetSliders();
+      if(shapes[shapeIndex].indices.length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
+      if(shapeIndex < shapes.length-1) shapeIndex++;
+      p.resetSliders();
+      console.log(shapeIndex);
     }
   }
+
   p.downIndex = function(){
     if(shapes[shapeIndex] != undefined){
-        if(shapes[shapeIndex].indices.length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
-        if(shapeIndex > 0) shapeIndex--;
-        p.resetSliders();
+      if(shapes[shapeIndex].indices.length == 0 && shapes.length > 1) shapes.splice(shapeIndex, 1);
+      if(shapeIndex > 0) shapeIndex--;
+      p.resetSliders();
+      console.log(shapeIndex);
     }
   }
 
