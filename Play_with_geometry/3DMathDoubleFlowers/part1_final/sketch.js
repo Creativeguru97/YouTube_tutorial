ef9dc3a8cf426705ae3a97bf7cf79d83ec534e8d
kazcodingburner <40142129+Creativeguru97@users.noreply.github.com>
2022-05-07 19:04:16 +0900

    Add sample code of the 3D Rose

---
+++ Play_with_geometry/3DMathDoubleFlowers/part1_final/sketch.js
@@ -0,0 +1,32 @@
+function setup(){
+  createCanvas(800, 800, WEBGL);
+  colorMode(HSB);
+  angleMode(DEGREES);
+  stroke(205, 50, 100);
+  strokeWeight(4);
+}
+
+function draw(){
+  background(230, 50, 15);
+  orbitControl(4, 4);//3D mouse control
+
+  rotateX(-30);
+
+  for(let r = 0; r <= 1; r += 0.03){
+    beginShape(POINTS);
+    for(let theta = -2*180; theta <= 180*15; theta += 3){
+      stroke(205, -r*50+100, r*50+50);
+      let phi = (180/2)*Math.exp(-theta/(8*180));
+      let petalCut = 1 - (1/2) * pow((5/4)*pow(1-((3.6*theta%360)/180), 2)-1/4, 2);
+      let hangDown = 2*pow(r, 2)*pow(1.3*r-1, 2)*sin(phi);
+
+      if(0 < petalCut * (r * sin(phi)+hangDown*cos(phi))){
+        let pX = 250 * petalCut * (r * sin(phi)+hangDown*cos(phi)) * sin(theta);
+        let pY = -250 * petalCut * (r * cos(phi)-hangDown*sin(phi));
+        let pZ = 250 * petalCut * (r * sin(phi)+hangDown*cos(phi)) * cos(theta);
+        vertex(pX, pY, pZ);
+      }
+    }
+    endShape();
+  }
+}
